d2ba14942fbff448640d60f1bdad633b5444ddbd
Maksims Tihomirovs <124576471+voyagerr666@users.noreply.github.com>
2023-04-17 00:40:00 +0300

    221RDB154 Lab.d. 3.2
    221RDB154 Lab.d. 3.2

--- hash_substring.py
+++ hash_substring.py
@@ -1,43 +1,49 @@
 # python3
 
+import os
+import sys
+
+
 def read_input():
-    t_in=input().strip().upper()
-    if t_in=="I":
-        pattern=input().strip()
-        text=input().strip()
-    elif t_in=="F":
-        filename='tests/06'
-        with open(filename) as file:
-            pattern=file.readline().strip()
-            text=file.readline().strip()
-            
+    t_in = input().strip().upper()
+    if t_in == "I":
+        pattern = input().strip()
+        text = input().strip()
+    elif t_in == "F":
+        filename = 'tests/06'
+        try:
+            with open(filename) as file:
+                pattern = file.readline().strip()
+                text = file.readline().strip()
+        except FileNotFoundError:
+            print("Error: file not found")
+            sys.exit(1)
     else:
-        exit()
-    return t_in, pattern,text          
-    
+        print("Error: invalid input choice")
+        sys.exit(1)
+    return t_in, pattern, text
+
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences( t_in,pattern, text):
-    text_l=len(text)
-    pattern_l=len(pattern)
-    occurances=[]
-    if t_in=="I":
-        for i in range(text_l-pattern_l+1):
-            if text[i: i+pattern_l]==pattern:
-                occurances.append(i)
-    elif t_in=="F":
-        pattern1=sum(ord(pattern[i])*pow(10,pattern_l-i-1) for i in range(pattern_l))
-        text1=sum(ord(text[i])*pow(10,pattern_l-i-1) for i in range(pattern_l))
-        for i in range(text_l-pattern_l+1):
-            if text1==pattern1 and text[i:i +pattern_l]==pattern:
-                occurances.append(i)
-            if i<text_l-pattern_l:
-                text1=(text1-ord(text[i])*pow(10,pattern_l-1))*10+ord(text[i+pattern_l]) 
-                                        
-    return occurances
-    
+
+def get_occurrences(t_in, pattern, text):
+    n = len(text)
+    m = len(pattern)
+    p = 31
+    p_pow = [pow(p, i) for i in range(m)]
+    pattern_hash = sum([ord(pattern[i]) * p_pow[m - 1 - i] for i in range(m)])
+    text_hash = sum([ord(text[i]) * p_pow[m - 1 - i] for i in range(m)])
+    matches = []
+    for i in range(n - m + 1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i + m]:
+                matches.append(i)
+        if i < n - m:
+            text_hash = (text_hash - ord(text[i]) * p_pow[m - 1]) * p + ord(text[i + m])
+    return matches
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
