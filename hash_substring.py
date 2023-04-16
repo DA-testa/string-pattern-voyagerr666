5a9eb414c838976b6a16c2d414026e7d5f2a8e8f
Maksims Tihomirovs <124576471+voyagerr666@users.noreply.github.com>
2023-04-16 21:56:18 +0300

    221RDB154 Lab.d. 3.2
    221RDB154 Lab.d. 3.2

--- hash_substring.py
+++ hash_substring.py
@@ -1,39 +1,56 @@
 # python3
 
-import sys
+def read_input():
 
-def hash_func(s, p):
-    p_len = len(p)
-    h = 0
-    for i in range(p_len):
-        h += ord(s[i]) * pow(len(s), p_len - i - 1)
-    return h % p_len
+    choice = input().strip()
 
-def get_occurrences(pattern, text):
-    p_len = len(pattern)
-    t_len = len(text)
-    p_hash = hash_func(text[:p_len], pattern)
-    result = []
-    for i in range(t_len - p_len + 1):
-        if p_hash == hash_func(text[i:i+p_len], pattern):
-            flag = True
-            for j in range(p_len):
-                if text[i+j] != pattern[j]:
-                    flag = False
-                    break
-            if flag:
-                result.append(i)
-    return result
+    if choice == 'I':
+        pattern = input().strip()
+        text = input().strip()
+    elif choice == 'F':
+        with open('test.txt', 'r') as file:
+            pattern = file.readline().strip()
+            text = file.readline().strip()
+    else:
+        raise ValueError('Invalid input choice')
+
+    return pattern, text
 
 def print_occurrences(output):
-    for i in output:
-        print(i, end=' ')
-    print()
+    print(' '.join(map(str, output)))
 
-def read_input():
-    pattern = input().rstrip()
-    text = input().rstrip()
-    return pattern, text
+def get_occurrences(pattern, text):
+    p = 10**9 + 7
+    x = 263
+    n = len(text)
+    m = len(pattern)
+    p_hash = [0] * (n - m + 1)
+    t_hash = [0] * (n - m + 1)
+    x_pow = [1] * (n - m + 1)
+
+    # Precompute hashes
+    p_hash[0] = hash(pattern, p, x)
+    t_hash[0] = hash(text[:m], p, x)
+
+    for i in range(1, n - m + 1):
+        p_hash[i] = (x * p_hash[i-1] + ord(pattern[i+m-1])) % p
+        t_hash[i] = (x * t_hash[i-1] + ord(text[i+m-1])) % p
+        x_pow[i] = (x * x_pow[i-1]) % p
+
+    occurrences = []
+    for i in range(n - m + 1):
+        if p_hash[i] != t_hash[i]:
+            continue
+        if text[i:i+m] == pattern:
+            occurrences.append(i)
+
+    return occurrences
+
+def hash(s, p, x):
+    h = 0
+    for c in reversed(s):
+        h = (h * x + ord(c)) % p
+    return h
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
