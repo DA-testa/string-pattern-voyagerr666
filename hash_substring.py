4db73483666f340f37e7e760a20d50b7274a2769
Maksims Tihomirovs <124576471+voyagerr666@users.noreply.github.com>
2023-04-16 13:46:35 +0300

    221RDB154 Lab.d. 3.2
    221RDB154 Lab.d. 3.2

--- hash_substring.py
+++ hash_substring.py
@@ -2,9 +2,10 @@
 
 import sys
 
+
 def read_input():
-    if sys.argv[1] == 'F':
-        with open('input.txt', 'r') as f:
+    if len(sys.argv) > 1 and sys.argv[1] == 'F':
+        with open('input.txt') as f:
             pattern = f.readline().rstrip()
             text = f.readline().rstrip()
     else:
@@ -12,23 +13,29 @@ def read_input():
         text = input().rstrip()
     return pattern, text
 
+
 def print_occurrences(output):
     print(' '.join(map(str, output)))
+
 
 def get_occurrences(pattern, text):
     occurrences = []
     p_len = len(pattern)
     t_len = len(text)
-    p_hash = sum([ord(pattern[i])*pow(10, p_len-i-1) for i in range(p_len)]) % 101
-    t_hash = sum([ord(text[i])*pow(10, p_len-i-1) for i in range(p_len)]) % 101
-    if t_hash == p_hash and text[:p_len] == pattern:
-        occurrences.append(0)
-    for i in range(1, t_len-p_len+1):
-        t_hash = (t_hash-ord(text[i-1])*pow(10, p_len-1))*10 + ord(text[i+p_len-1])
-        if t_hash == p_hash and text[i:i+p_len] == pattern:
-            occurrences.append(i)
+
+    p_hash = sum(ord(pattern[i]) * pow(10, p_len - i - 1) for i in range(p_len))
+
+    t_hash = sum(ord(text[i]) * pow(10, p_len - i - 1) for i in range(p_len))
+
+    for i in range(t_len - p_len + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+p_len]:
+                occurrences.append(i)
+        if i < t_len - p_len:
+            t_hash = (t_hash - ord(text[i]) * pow(10, p_len - 1)) * 10 + ord(text[i+p_len])
+
     return occurrences
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
