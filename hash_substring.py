ee6bea3f5f721a6ab41e9c4c3e3b1adfed33b209
Maksims Tihomirovs <124576471+voyagerr666@users.noreply.github.com>
2023-04-16 13:52:40 +0300

    221RDB154 Lab.d. 3.2
    221RDB154 Lab.d. 3.2

--- hash_substring.py
+++ hash_substring.py
@@ -2,40 +2,38 @@
 
 import sys
 
-
-def read_input():
-    if len(sys.argv) > 1 and sys.argv[1] == 'F':
-        with open('input.txt') as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
-    else:
-        pattern = input().rstrip()
-        text = input().rstrip()
-    return pattern, text
-
-
-def print_occurrences(output):
-    print(' '.join(map(str, output)))
-
+def hash_func(s, p):
+    p_len = len(p)
+    h = 0
+    for i in range(p_len):
+        h += ord(s[i]) * pow(len(s), p_len - i - 1)
+    return h % p_len
 
 def get_occurrences(pattern, text):
-    occurrences = []
     p_len = len(pattern)
     t_len = len(text)
-
-    p_hash = sum(ord(pattern[i]) * pow(10, p_len - i - 1) for i in range(p_len))
-
-    t_hash = sum(ord(text[i]) * pow(10, p_len - i - 1) for i in range(p_len))
-
+    p_hash = hash_func(text[:p_len], pattern)
+    result = []
     for i in range(t_len - p_len + 1):
-        if p_hash == t_hash:
-            if pattern == text[i:i+p_len]:
-                occurrences.append(i)
-        if i < t_len - p_len:
-            t_hash = (t_hash - ord(text[i]) * pow(10, p_len - 1)) * 10 + ord(text[i+p_len])
+        if p_hash == hash_func(text[i:i+p_len], pattern):
+            flag = True
+            for j in range(p_len):
+                if text[i+j] != pattern[j]:
+                    flag = False
+                    break
+            if flag:
+                result.append(i)
+    return result
 
-    return occurrences
+def print_occurrences(output):
+    for i in output:
+        print(i, end=' ')
+    print()
 
+def read_input():
+    pattern = input().rstrip()
+    text = input().rstrip()
+    return pattern, text
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
